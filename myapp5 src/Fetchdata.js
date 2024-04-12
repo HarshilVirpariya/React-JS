badfa7656161b63e538478e515597451f830cd63
HarshilVirpariya <152393055+HarshilVirpariya@users.noreply.github.com>
2024-04-12 17:30:37 +0530

    React Day - 5

---
+++ myapp5 src/Fetchdata.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState , useEffect } from 'react'
+
+function Fetching()
+{
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchingdata = async () => {
+            try {
+                let API = await fetch("https://fakestoreapi.com/products");
+
+                let jsondata = await API.json();
+                setData(jsondata);
+                console.log(jsondata);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchingdata();
+        
+    }, []);
+    return (
+        <div className="container" >
+        {data.map((el, index) => (
+            <div key={index} className="box" >
+                <img src={el.image} alt="Product Image" />
+                <p>{el.title}</p>
+                <h3> $ : {el.price}</h3>
+            </div>
+        ))}
+    </div>
+  )
+}
+export default Fetching
