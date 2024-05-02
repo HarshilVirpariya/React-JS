78180e29edb998b6edd2ac936867724a457fe736
HarshilVirpariya <152393055+HarshilVirpariya@users.noreply.github.com>
2024-05-02 17:29:59 +0530

    React Day - 18

---
+++ myapp18/src/Template.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Nav from './Componants/Nav'
+import Header from './Componants/Header'
+import Section1 from './Componants/Section1'
+import Section2 from './Componants/Section2'
+import Section3 from './Componants/Section3'
+import Section4 from './Componants/Section4'
+import Footer from './Componants/Footer'
+
+function Template() {
+  return (
+    <>
+        <Nav/>
+        <Header/>
+        <Section1/>
+        <Section2/>
+        <Section3/>
+        <Section4/>
+        <Footer/>
+    </>
+  )
+}
+
+export default Template
